6ee5c40ead56dd6cdfa4896097d48f9fae03625c
Andrea <82764646+Dozzy85@users.noreply.github.com>
2022-06-22 15:44:24 +0200

    Initial commit

---
+++ InputOutput.py
@@ -0,0 +1,35 @@
+print("Ciao Python")
+
+print(5)
+print(2.3)
+
+#somma
+print(5+3)
+
+#sottrazione
+print(10-2.2)
+
+#moltiplicazione
+print(10*3)
+
+#divisione
+print(10/3)
+
+#divisione (solo parte intera)
+print(10//3)
+
+#resto della divisione
+print(10%3)
+
+#potenza
+print(10**2)
+
+#unire le 2 stringhe
+print("5"+"2")
+print("Ciao"+" "+"Python")
+
+#possiamo farlo anche separando i dati con la virgola
+print("Ciao","Python")
+
+#Input da tastiera e per salvare il dato dobbiamo usare una variabile
+name=input("Come ti chiami ? ")
---
+++ VariabiliTipiVariabili.py
@@ -0,0 +1,34 @@
+#i nomi delle variabili li scegliamo noi ed è buona
+# norma sceglierne uno che ci permetta di ricordare il contenuto di una variabile
+name=input("Come ti chiami ? ")
+print(name)
+
+grandpa_age=input("Quanti anni ha tuo nonno ? ")
+
+var="ciao"
+print(type(var))
+print(var)
+
+var=3.5
+print(type(var))
+print(var)
+
+var=False
+print(type(var))
+print(var)
+
+#cast delle variabili
+var=5
+var=str(5)
+print(type(var))
+print(var)
+
+var="10"
+var=int(var)
+print(type(var))
+print(var)
+
+var="10.5"
+var=float(var)
+print(type(var))
+print(var)
